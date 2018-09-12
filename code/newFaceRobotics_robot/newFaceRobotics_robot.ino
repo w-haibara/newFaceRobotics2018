ea389785225cef6aa3f8550a6006a8b56e93a79c
w <hwhaibarawataru@gmail.com>
2018-09-12 18:53:20 +0900

    モーターの正転回転とpwmを追加

--- code/newFaceRobotics_robot/newFaceRobotics_robot.ino
+++ code/newFaceRobotics_robot/newFaceRobotics_robot.ino
@@ -7,30 +7,32 @@
 #include <Servo.h>
 
 IPAddress myIP(192, 168, 4, 1);
-const char *ssid = "AoD_ESP_001";sudo
+const char *ssid = "AoD_ESP_001";
 const char *password = "AoDESP001";
 
-byte WiFibuff[1];
+byte WiFibuffer[1];
 WiFiUDP UDP;
 
 Servo servo;
 
+const uint8_t servoPin = 0;
 
-const int servoPin = 0;
+const uint8_t gearboxPin = 1;
 
-const int gearboxPin = 1;
+const uint8_t omniPin_A1 = 2;
+const uint8_t omniPin_A2 = 3;
 
-const int omniPin_A1 = 2;
-const int omniPin_A2 = 3;
+const uint8_t omniPin_B1 = 4;
+const uint8_t omniPin_B2 = 5;
 
-const int omniPin_B1 = 4;
-const int omniPin_B2 = 5;
+const uint8_t omniPin_C1 = 12;
+const uint8_t omniPin_C2 = 13;
 
-const int omniPin_C1 = 12;
-const int omniPin_C2 = 13;
+const uint8_t omniPin_D1 = 13;
+const uint8_t omniPin_D2 = 15;
 
-const int omniPin_D1 = 13;
-const int omniPin_D2 = 15;
+
+boolean motorDirection = false;
 
 void setup() {
   Serial.begin(115200);
@@ -45,18 +47,80 @@ void setup() {
   pinMode(servoPin, OUTPUT);
   pinMode(gearboxPin, OUTPUT);
 
-  pinMode(omniPinA1, OUTPUT);
-  pinMode(omniPinA2, OUTPUT);
+  pinMode(omniPin_A1, OUTPUT);
+  pinMode(omniPin_A2, OUTPUT);
 
-  pinMode(omniPinB1, OUTPUT);
-  pinMode(omniPinB2, OUTPUT);
+  pinMode(omniPin_B1, OUTPUT);
+  pinMode(omniPin_B2, OUTPUT);
 
-  pinMode(omniPinC1, OUTPUT);
-  pinMode(omniPinC2, OUTPUT);
+  pinMode(omniPin_C1, OUTPUT);
+  pinMode(omniPin_C2, OUTPUT);
 
-  pinMode(omniPinD1, OUTPUT);
-  pinMode(omniPinD2, OUTPUT);
+  pinMode(omniPin_D1, OUTPUT);
+  pinMode(omniPin_D2, OUTPUT);
 }
 
 void loop() {
+  if (UDP.parsePacket() > 0) {
+    receiveWiFi();
+  }
 }
+
+void receiveWiFi() {
+  UDP.read(WiFibuffer, 1);
+  Serial.write(WiFibuffer[0]);
+  UDP.flush();
+}
+
+void driveOmni(float x , float y) {
+
+}
+
+void motorControl(char omniName, int velocity) {
+  //omniNameでA,B,C,Dのどのオムニのモーターかを，velosityで-255～255の範囲の速度を指定
+  uint8_t HbridgeR = 255;
+  uint8_t HbridgeL = 255;
+  uint8_t pwm  = abs(velocity);
+  boolean minus =  (velocity < 0) ? true : false;
+
+  //minus =  !minus;をコメントアウトすることで各モーターの回転方向を反転させる
+  switch (omniName) {
+    case 'A':
+      HbridgeR = omniPin_A1;
+      HbridgeL = omniPin_A2;
+      //minus =  !minus;
+      break;
+    case 'B':
+      HbridgeR = omniPin_B1;
+      HbridgeL = omniPin_B2;
+      //minus =  !minus;
+      break;
+    case 'C':
+      HbridgeR = omniPin_C1;
+      HbridgeL = omniPin_C2;
+      //minus =  !minus;
+      break;
+    case 'D':
+      HbridgeR = omniPin_D1;
+      HbridgeL = omniPin_D2;
+      //minus =  !minus;
+      break;
+    default:
+      break;
+  }
+
+  digitalWrite(HbridgeR , LOW);
+  digitalWrite(HbridgeL , LOW);
+
+  if (motorDirection != minus) delay(1); //モーターの回転方向が変わるときには1ms待つ(fetで信号が遅延するため)
+
+  if (minus) {
+    //モーターが逆転する処理(R→L方向への電流)
+    analogWrite(HbridgeR , pwm);
+  } else {
+    //モーターが正転する処理(L→R方向への電流)
+    analogWrite(HbridgeL , pwm);
+  }
+  motorDirection = minus;
+}
+
